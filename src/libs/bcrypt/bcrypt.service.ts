c4a319ee2f7c4ea1e609ebff3897872a652e4c31
Maksim Kropanov <kropanov@gmail.com>
2024-11-15 21:49:18 +0300

    feat: implement bcrypt service

---
+++ src/libs/bcrypt/bcrypt.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import bcrypt from 'bcrypt';
+
+@Injectable()
+export class BcryptService {
+    saltRounds = 10;
+
+    async hash(plainTextPassword: string): Promise<string> {
+        try {
+            return await bcrypt.hash(plainTextPassword, this.saltRounds);
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
+
+    async compare(plainTextPassword: string, hash: string): Promise<boolean> {
+        try {
+            return await bcrypt.compare(plainTextPassword, hash);
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
+}
--- src/main.module.ts
+++ src/main.module.ts
@@ -1,3 +1,4 @@
+import { BcryptModule } from '@app/libs/bcrypt/bcrypt.module';
 import { PrismaModule } from '@app/libs/prisma';
 import { AuthModule } from '@app/modules/auth/auth.module';
 import { ItemsModule } from '@app/modules/items/items.module';
@@ -5,7 +6,7 @@ import { UsersModule } from '@app/modules/users';
 import { Module } from '@nestjs/common';
 
 @Module({
-    imports: [PrismaModule, AuthModule, UsersModule, ItemsModule],
+    imports: [PrismaModule, AuthModule, UsersModule, ItemsModule, BcryptModule],
     providers: [],
 })
 export class AppModule {}
