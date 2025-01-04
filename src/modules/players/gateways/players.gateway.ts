e61c774ad8f38eb490799c1a32652942c840c2bb
Maksim Kropanov <kropanov@gmail.com>
2025-01-04 23:58:32 +0300

    refactor: player gateway

--- src/modules/players/gateways/players.gateway.ts
+++ src/modules/players/gateways/players.gateway.ts
@@ -1,9 +1,8 @@
+import { PlayerEvents, PlayerResponseEvents } from '@app/enums';
 import {
     ConnectedSocket,
     MessageBody,
-    OnGatewayConnection,
     OnGatewayDisconnect,
-    OnGatewayInit,
     SubscribeMessage,
     WebSocketGateway,
     WebSocketServer,
@@ -14,58 +13,40 @@ import { CreatePlayerDto } from '../dto/create-player.dto';
 import { PlayersService } from '../services/players.service';
 
 @WebSocketGateway(5000, { cors: { origin: process.env.CLIENT_URL || 'http://localhost:5173' } })
-export class PlayersGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+export class PlayersGateway implements OnGatewayDisconnect {
     @WebSocketServer()
     server: Server;
 
     constructor(private readonly playersService: PlayersService) {}
 
-    handleConnection(client: any) {
-        console.log(`Client connected: ${client.id}`);
-    }
-
     handleDisconnect(client: any) {
-        console.log(`Client disconnected: ${client.id}`);
         this.playersService.remove(client.id);
-        this.server.send({ type: 'player_leave', data: { clientId: client.id } });
+        this.server.send({ type: PlayerResponseEvents.LEFT, data: { clientId: client.id } });
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    afterInit(_server: any): any {
-        console.log('WebSocket Server Initialized');
-    }
-
-    @SubscribeMessage('joined')
+    @SubscribeMessage(PlayerEvents.JOIN)
     create(@MessageBody() createPlayerDto: CreatePlayerDto, @ConnectedSocket() client: Socket) {
-        console.log('id:', createPlayerDto.id);
-        console.log('clientid:', client.id);
-
-        // send message to clients
-        // this.server.send({ type: 'create', data: 123 });
-        console.log();
         const newPlayer = this.playersService.joined(createPlayerDto, client.id);
-        this.server.send({ type: 'new_player', data: newPlayer });
+        this.server.send({ type: PlayerResponseEvents.JOINED, data: newPlayer });
     }
 
-    @SubscribeMessage('findAllPlayers')
-    findAll() {
-        const data = this.playersService.getAllPlayers();
-
-        return [...data];
+    @SubscribeMessage(PlayerEvents.GET_ALL)
+    getAllPlayers() {
+        return this.playersService.getAllPlayers();
     }
 
-    @SubscribeMessage('move')
-    findOne(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
-        const player = this.playersService.move(data, client.id);
+    @SubscribeMessage(PlayerEvents.ACTION)
+    broadcastPlayerAction(@MessageBody() body: any, @ConnectedSocket() client: Socket) {
+        // Save here current player position
+        // this.playersService.setPlayerPosition
 
-        this.server.send({ type: 'move', data: player });
-    }
-
-    @SubscribeMessage('playerAction')
-    remove(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
         this.server.send({
-            type: 'player_move',
-            data: { action: data.action, keyCode: data.keyCode, clientId: client.id },
+            type: PlayerResponseEvents.ACTION_PERFORMED,
+            data: {
+                action: body.action,
+                keyCode: body.keyCode,
+                clientId: client.id,
+            },
         });
     }
 }
